cf4717f8ca6e95d403cd9b0e3debe6fbee2fd4a6
Tuki1077 <70412089+Tuki1077@users.noreply.github.com>
2022-04-04 18:09:14 -0600

    Update B+.py

--- B+.py
+++ B+.py
@@ -302,13 +302,21 @@ def printTree(tree):
 
 record_len = 3
 bplustree = BplusTree(record_len)
+print ('Insertion')
 bplustree.insert('5', '33')
 bplustree.insert('15', '21')
 bplustree.insert('25', '31')
 bplustree.insert('35', '41')
 bplustree.insert('45', '10')
 bplustree.insert('50', '15')
-
+printTree(bplustree)
+if(bplustree.find('5', '33')):
+    print("Found")
+else:
+    print("Not found")
+    
+bplustree.delete('5', '33')
+print('Ejemplo delete')
 printTree(bplustree)
 
 if(bplustree.find('5', '33')):
